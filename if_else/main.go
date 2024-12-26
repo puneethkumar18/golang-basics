e6b2a239ff25c2f1b0050bca5ad9c13583e67f9f
PUNEETH KUMAR <puneethkumarg96@gmail.com>
2024-12-26 19:33:03 +0530

    if-else

---
+++ if_else/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("We Are Starting If Else Statements")
+
+	num := 11
+
+	if num > 10 {
+		fmt.Println(num, "is Number is Greater than 10")
+	} else {
+		fmt.Println(num, "is Number is Less Thane 10 ")
+	}
+}
