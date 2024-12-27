20e81b995b0f3e11d7f98dc095520747ebaae372
PUNEETH KUMAR <puneethkumarg96@gmail.com>
2024-12-27 09:20:11 +0530

    maps

--- mapsInGo/main.go
+++ mapsInGo/main.go
@@ -9,11 +9,23 @@ func main() {
 
 	var map1 = map[string]int{"Puneeth": 1, "Raj": 2, "Ravi": 3}
 
-	var map2 = map[string]int{"Puneeth": 1, "Raj": 2, "Ravi": 3}
+	map1["Abhi"] = 4
+
+	var map2 = map[string]int{"Puneeth": 10, "Raj": 2, "Ravi": 3, "Abhi": 10000}
+
+	map2["Ramesh"] = 420
 	// using make function
 	var myMap = make(map[string]int)
 	myMap["Kumar"] = 1
 
 	fmt.Println(myMap, map1, map2)
+
+	for key, value := range map2 {
+		fmt.Println("key is ", key, "Value is ", value)
+	}
+
+	for key, value := range map1 {
+		fmt.Println("key is ", key, "Value is ", value)
+	}
 
 }
